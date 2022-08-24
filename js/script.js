4bb450a1d053705e6bc4fbdba4d58acb34ccd635
pf4pad <65763670+pf4pad@users.noreply.github.com>
2022-08-24 11:15:18 +0300

    day3

--- js/script.js
+++ js/script.js
@@ -1,28 +1,79 @@
 const buttonMen = document.querySelector('.header__button-men')
 const buttonWomen = document.querySelector('.header__button-women')
 const body = document.body
+const cardImage = document.querySelector('.card__image')
+const cardText = document.querySelector('.card__text')
 
-let gender = 'women'
+const buttonText = document.querySelector('.header__button-change_text')
+const buttonImage = document.querySelector('.header__button-change_image')
+
+const state = {
+  gender: body.classList.contains('women') ? 'women' : 'men'
+}
+
+const changeDom = () => {
+  if (state.photo.includes('black')) {
+    cardText.style.color = '#fff'
+  } else {
+    cardText.style.color = ''
+  }
+  cardImage.src = `/img/${state.photo}`
+  cardText.innerHTML = state.text.replaceAll('\n', '<br>')
+}
+
+const getData = () => fetch('db.json')
+  .then(response => response.json())
+
+const getRandomForArr = (arr) => {
+  const randomNumber = Math.floor(Math.random() * arr.length)
+  return arr[randomNumber]
+}
+const getDataCard = () => {
+  getData().then(data => {
+    state.text = getRandomForArr(data.text[state.gender])
+    state.photo = getRandomForArr(data.photo[state.gender])
+
+    changeDom()
+
+  })
+
+}
 const changeToMen = () => {
-  if (gender !== 'men') {
+  if (state.gender !== 'men') {
     body.classList.add('men')
-    buttonMen.classList.add('header__button-men_active')
     body.classList.remove('women')
-    buttonWomen.classList.remove('header__button-women_active')
-    gender = 'men'
+    state.gender = 'men'
+    getDataCard()
   }
 
 }
 const changeToWomen = () => {
-  if (gender !== 'women') {
+  if (state.gender !== 'women') {
     body.classList.add('women')
-    buttonWomen.classList.add('header__button-women_active')
     body.classList.remove('men')
-    buttonMen.classList.remove('header__button-men_active')
-    gender = 'women'
+    state.gender = 'women'
+    getDataCard()
   }
 
 }
 
+const changeText = () => {
+  getData().then(data => {
+    state.text = getRandomForArr(data.text[state.gender])
+    changeDom()
+  })
+}
+const changeImage = () => {
+  getData().then(data => {
+
+    state.photo = getRandomForArr(data.photo[state.gender])
+    changeDom()
+
+  })
+}
+
 buttonMen.addEventListener('click', changeToMen)
 buttonWomen.addEventListener('click', changeToWomen)
+buttonText.addEventListener('click', changeText)
+buttonImage.addEventListener('click', changeImage)
+getDataCard()
