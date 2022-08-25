b0f96992e4b7df961947f25056426276b501313a
pf4pad <65763670+pf4pad@users.noreply.github.com>
2022-08-25 13:53:39 +0300

    save_image2

--- js/script.js
+++ js/script.js
@@ -95,5 +95,32 @@ cardSave.addEventListener('click', () => {
     canvas.style.maxWidth = '100%'
     canvas.style.heigth = 'auto'
     newWindow.document.body.append(canvas)
+    saveAs(canvas.toDataURL(), 'file-name.png');
+
   })
 })
+function saveAs(uri, filename) {
+
+  var link = document.createElement('a');
+
+  if (typeof link.download === 'string') {
+
+    link.href = uri;
+    link.download = filename;
+
+    //Firefox requires the link to be in the body
+    document.body.appendChild(link);
+
+    //simulate click
+    link.click();
+
+    //remove the link when done
+    document.body.removeChild(link);
+
+  } else {
+
+    window.open(uri);
+
+  }
+}
+
